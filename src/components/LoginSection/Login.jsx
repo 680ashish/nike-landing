aa3b7406894d2b5cce9811a901648ca46a33bd56
680ashish <680ashish@gmail.com>
2024-04-02 00:46:54 +0600

    login page added

--- src/components/LoginSection/Login.jsx
+++ src/components/LoginSection/Login.jsx
@@ -1,21 +1,57 @@
-import React from 'react'
-import './Login.css'
+import React, { useState } from 'react'
+import styles from './Login.module.css';
+// import userIcon from './person.png';
+// import emailIcon from './email.png';
+// import pwdIcon from './password.png';
 const Login = ({handleLoginp}) => {
 
+  const [action,setAction] = useState("Log In");
   return (
     <>
-    
-    <div className='login'>Login</div>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, excepturi accusamus. Placeat earum rem, accusamus inventore rerum alias beatae cumque autem animi magni voluptatibus repellendus laborum ipsam corporis maiores illum!
-      Laudantium quo magnam odit inventore eaque, tempore est minus sint explicabo quis illo adipisci, odio ratione at ducimus accusamus laborum alias ex voluptate soluta. Veritatis, maxime. Ullam suscipit placeat commodi.
-      Maxime deserunt libero eos itaque quia quos! Odio facere magnam iusto neque illo voluptates ab aperiam eos natus! Doloribus ducimus soluta, eius eum illo beatae provident dolore atque voluptatibus quo!
-      Nesciunt unde dolore id doloremque placeat illo eius. Sunt natus numquam molestiae dolorum odit rem magnam nulla laudantium officiis qui eum ipsum cum mollitia, deserunt fugit voluptate, minus culpa pariatur?
-      Voluptas commodi distinctio tenetur quo? Cupiditate magnam architecto dolorem quisquam accusantium porro reiciendis omnis quo qui perferendis doloribus tenetur dolore nobis deserunt earum ipsam recusandae consequuntur explicabo aliquid, accusamus voluptas.
-    </p>
-    <button onClick={handleLoginp}>
-      Submit
-    </button>
+    <div className='loginBox'>
+    <div className={` ${styles.logincontainer} `}>
+      <div className={`${styles.header}`}>
+        <div className={`${styles.text}`}>{action}</div>
+        <div className="underline"></div>
+
+      </div>
+      <div className={`${styles.inputs}`}>
+        {action==="Log In"?<div></div>:
+        <div className={`${styles.input}`}>
+        <img src="person.png" alt="person" className={`${styles.img}`}/>
+        <input type="text" placeholder='Enter username'/>
+      </div>  
+        }
+        
+        <div className={`${styles.input}`}>
+          <img src="email.png" alt="email" className={`${styles.img}`}/>
+          <input type="email" placeholder='Enter your email'/>
+        </div>
+        
+        <div className={`${styles.input}`}>
+          <img src="password.png" alt="password" className={`${styles.img}`}/>
+          <input type="password" placeholder='Enter your password'/>
+        </div>
+      </div>
+      {
+        action==="Sign Up"?<div></div>:
+      <div className={`${styles.faggot}`}>
+        Lost Password?
+      </div>
+      }
+      <div className={`${styles.submitContainer}`}>
+        <div className={action==="Log In"?`${styles.submit} ${styles.gray}`:`${styles.submit}`} onClick={()=>{setAction("Sign Up")}}>
+          Sign Up
+        </div>
+        <div className={action==="Log In"?`${styles.submit} `:`${styles.submit} ${styles.gray}`}  onClick={()=>{setAction("Log In")}}>
+          Log In
+        </div>
+      </div>
+    </div>
+
+
+
+    </div>
 
     </>
   )
